77597e65a810a584604e3e9b833e9bd32ace89e1
Wren <grahamgrasley@gmail.com>
2018-09-12 15:30:22 -0400

    Fixes and improvements
    Fixed issues with new-caption detection, removed unnecessary code.

--- t_unnest.js
+++ t_unnest.js
@@ -1,19 +1,31 @@
 function unnest(boxName, avatarsize, showcolons, wrapclass, addnewname, openintab) {
 	document.addEventListener("DOMContentLoaded",function(){
-	let host = window.location.hostname.split(".").slice(0, -2);
+	let host = String(window.location.hostname.split(".").slice(0, -2));
 		
 	let containsPost = function(elem) {
-		if (elem.children.length>1 && elem.children[1].children.length>0 ){
+		if (elem.children.length>1 && elem.children[1].children.length>0){
 			let e = elem.children[1].children[0];
 			return (e.children.length>0 && e.children[0].classList.contains("tumblr_blog"));
 		} else { return false; }
 	}
 	
 	let makeName = function(elem) {
-		elem.className="tumblr_blog";
-			if (!showcolons) {
-				elem.innerHTML = elem.innerHTML.slice(0, -1);
+		if(typeof elem === "string"){
+			let e = document.createElement("p");
+			document.body.appendChild(e);
+			e.innerHTML="<a href='http://"+elem+".tumblr.com>"+elem+"</a>:";
+			let a = document.createElement("a");
+			a.setAttribute("href","http://"+elem+".tumblr.com");
+			a.appendChild(document.createTextNode(elem));
+			e.appendChild(a);
+			if(showcolons){
+				e.appendChild(document.createTextNode(":"));
 			}
+			elem = e;
+		} else if (!showcolons){
+			elem.innerHTML = elem.innerHTML.slice(0, -1);
+		}
+		elem.className="tumblr_blog";
 			elem.children[0].removeAttribute("class");
 			if(openintab){
 				elem.children[0].setAttribute("target","_blank");
@@ -38,10 +50,11 @@ function unnest(boxName, avatarsize, showcolons, wrapclass, addnewname, openinta
 		let is_bottom = !containsPost(curblock);
 		if ((curblock.children.length > 2 && !is_bottom) || is_bottom) {
 			let el = document.createElement("div");
+			main.appendChild(el);
 			el.className=wrapclass;
 			if (is_bottom) {
 				let isnew = true;
-				if(curblock.children.length>1){
+				if(curblock.children.length==2){
 					let ch = curblock.children[0];
 					isnew = !(ch.children.length>0 && ch.children[0].classList.contains("tumblr_blog") && curblock.children[1].tagName=="BLOCKQUOTE");
 				} else {
@@ -50,33 +63,34 @@ function unnest(boxName, avatarsize, showcolons, wrapclass, addnewname, openinta
 				if(!isnew){
 					el.appendChild(makeName(curblock.children[0]));
 					el.innerHTML += curblock.children[0].innerHTML;
+				} else if(curblock.children.length>2) {
+					el.insertAdjacentHTML("afterbegin",curblock.children[1].innerHTML);
+					el.insertBefore(makeName(curblock.children[0]),el.childNodes[0]);
+					el = document.createElement("div")
+					el.className=wrapclass;
+					main.appendChild(el);
+					el.appendChild(makeName(host));
+					let len = curblock.children.length;
+					for (let i = 1; i < len; i++) {
+						el.appendChild(curblock.children[1]);
+					}
 				} else {
 					if (addnewname) {
-						let ll = document.createElement("p");
-						ll.className = "tumblr_blog";
-							if (avatarsize > 0) {
-								let imgpath = "http://api.tumblr.com/v2/blog/" + host + "/avatar/" + String(avatarsize);
-							ll.innerHTML = "<img class='reblog_avatar' src='" + imgpath + "' data-lightbox='none' class='reblog_avatar'/> ";
-							}
-							ll.innerHTML += "<a href='javascript:void(0)'>" + host + "</a>";
-							if (showcolons) {
-								ll.innerHTML += ":";
-							}	
-							el.appendChild(ll);
+						el.appendChild(makeName(host));
 					}
 					el.innerHTML += curblock.innerHTML;
 				}
 			} else {
 				let len = curblock.children.length;
+				el.appendChild(makeName(host))
 				for (let i = 2; i < len; i++) {
 					el.appendChild(curblock.children[2]);
 				}
 			}
-			main.appendChild(el);
 		}
 		while (!is_bottom) {
 			is_bottom = !containsPost(curblock);
-			el = document.createElement("div");
+			let el = document.createElement("div");
 			el.className = wrapclass;
 			let n = curblock.children[0];
 			el.appendChild(makeName(n));
